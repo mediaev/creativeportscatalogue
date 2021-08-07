a1fd1bffb85eacf12f2dbbd2705f30b1ac06ad54
Simon Ljungberg <hi@iamsim.me>
2021-08-07 11:00:24 +0200

    Add foot note support

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,26 @@ menuToggle.addEventListener('click', (e) => {
   menuToggle.classList.toggle('active');
   menu.classList.toggle('hidden');
 });
+
+document.querySelectorAll('.footnote-item p').forEach((footnote) => {
+  const parent = footnote.parentNode;
+  const id = parent.id;
+  const copy = footnote.cloneNode(true);
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('footnote-reference', 'hidden');
+  wrapper.appendChild(copy);
+  const backLink = wrapper.querySelector('.footnote-backref')
+  backLink.parentNode.removeChild(backLink);
+
+  const link = document.querySelector('a[href="#' + id + '"]');
+  const referencing = link.closest('p');
+  referencing.insertAdjacentElement('afterend', wrapper);
+
+  link.addEventListener('mouseover', () => {
+    wrapper.classList.remove('hidden');
+  });
+
+  link.addEventListener('mouseout', () => {
+    wrapper.classList.add('hidden');
+  });
+});
